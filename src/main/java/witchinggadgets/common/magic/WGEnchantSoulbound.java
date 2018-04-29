82feb8be7ec666fdbfc48b075717b8b3ce419cad
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2018-04-29 19:25:28 +0200

    added missing soulbound class due to faulty .gitignore

---
+++ src/main/java/witchinggadgets/common/magic/WGEnchantSoulbound.java
@@ -0,0 +1,39 @@
+package witchinggadgets.common.magic;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.item.ItemStack;
+
+public class WGEnchantSoulbound extends Enchantment {
+
+	public static int id;
+	public WGEnchantSoulbound(int id) {
+		super(id, 0, EnumEnchantmentType.all);
+		this.setName("Soul Tether");
+		this.id=id;
+	}
+	
+	@Override
+	public int getMinEnchantability(int lvl)
+	{
+		return 6;
+	}
+
+	@Override
+	public int getMaxEnchantability(int lvl)
+	{
+	    return getMinEnchantability(lvl) + 20;
+	}
+
+	@Override
+	public int getMaxLevel()
+	{
+		return 1;
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack)
+	{
+		return false;
+	}
+}
